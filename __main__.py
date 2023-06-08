29d47ea80d3bb0789168c799ca6a75f3a537fc2f
Bahman <bahmanahmadi.mail@gmail.com>
2023-06-08 15:16:39 +0330

    bugs fixed:\n• edit bug: were seperate by <space> this can make problem in long texts\n• decryption bug: a char (ا) were repeat in two key (3 & 8)

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 from db import get, set as store, bcolors as C
 from numlang import decrypt, encrypt, EN, FA
 from hashlib import md5
+from random import sample
 from rich import print
 from re import findall
 
@@ -8,8 +9,19 @@ print("[green]welcome to [b]Numlang[/b] project[/green]")
 lang: str = input("select a lang:\n[1] EN\n[2] FA\n\n(pass = EN)>>> ")
 lang: dict = FA if lang.upper() == "FA" or "2" in lang else EN
 
+
+def generateKey(length=5) -> str:
+    def rnd():
+        return "".join(sample([*"0123456789abcdefghijklmnopqrstuvwxyz"], length))
+    res = rnd()
+    while get(res) is not None:
+        res = rnd()
+    return res
+
+
 while True:
     try:
+        # Help: for edit you'll have to seperate the parts with '~' and also don't encrypt new data manually; i.e. "1xy84~new value"
         text = input(f"{C.BOLD+C.OKCYAN}>>>{C.ENDC} ").lower()
 
         if text != "":
@@ -35,9 +47,10 @@ while True:
             if save == "r":
                 saveRes = get(text)
             elif save == "w":
-                saveRes = store(md5(result.encode()).hexdigest(), result)
+                saveRes = store(generateKey(), result)
             elif save == "e":
-                saveRes = store(*text.split())
+                sections = text.split("~")
+                saveRes = store(sections[0], encrypt('~'.join(sections[1:])))
             else:
                 ...
 
